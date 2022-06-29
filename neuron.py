1f59ced40548bdc75dfdac635609c5c2e7ce1489
ERR403 <91799709+ri-Soul@users.noreply.github.com>
2022-06-29 18:14:33 +0200

    EVERYTHING

--- neuron.py
+++ neuron.py
@@ -1,1 +1,33 @@
+import time
+import random
 
+while True:
+    game_mode = input("Enter diffeculty E-azy/N-ormal/H-ared")
+    if game_mode == "E":
+        game_mode = 6
+        break
+    elif game_mode == "N":
+        game_mode = 12
+        break
+    elif game_mode == "H":
+        game_mode = 15
+        break
+    else:
+        continue
+
+def gen():
+    numb = ""
+    for x in game_mode
+      numb += random.randInt(9+1)
+    return numb
+
+while True:
+    numb = gen()
+    print(numb)
+    time.sleep(4)
+    for x in range(256):
+        print()
+    if numb == input("Enter the number: "):
+        print("Correct!")
+    else:
+        print("Fault...")
